2502a45e11fbcb07cbc88fde6bd830d03af69ee9
Pavlo Novykov <pawelfcdd@gmail.com>
2024-01-16 16:48:44 +0100

    create_job_app: modify asserts

--- app/src/Controller/JobApplication/Create/JobApplicationCreateRequest.php
+++ app/src/Controller/JobApplication/Create/JobApplicationCreateRequest.php
@@ -11,13 +11,27 @@ readonly class JobApplicationCreateRequest
 {
     public function __construct(
         #[Assert\NotBlank]
+        #[Assert\Length(
+            min: 2,
+            max: 50,
+            maxMessage: 'The first name must be at most {{ limit }} characters long'
+        )]
         public string $firstName,
         #[Assert\NotBlank]
+        #[Assert\Length(
+            min: 2,
+            max: 50,
+            maxMessage: 'The last name must be at most {{ limit }} characters long'
+        )]
         public string $lastName,
         #[Assert\NotBlank]
         #[Assert\Email]
         public string $email,
         #[Assert\NotBlank]
+        #[Assert\Regex(
+            pattern: '/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/',
+            message: 'The phone number is not valid'
+        )]
         public string $phone,
         #[Assert\NotBlank]
         public int $expectedSalary,
